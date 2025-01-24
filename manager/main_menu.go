d9693e6d9df77f5cee54246b0f6907a4d48a33af
AvilaAndre <avila_andre14@hotmail.com>
2025-01-24 23:21:46 +0000

    chore: migrate to godot

--- manager/main_menu.go
+++
@@ -1,7 +0,0 @@
-package manager
-
-import rl "github.com/gen2brain/raylib-go/raylib"
-
-func draw_main_menu() {
-	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-}
