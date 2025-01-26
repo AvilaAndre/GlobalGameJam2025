43c169a4c96e2a75392a0f51911511bf43b0125e
TiagoMRib <tiago18.2002@gmail.com>
2025-01-26 13:25:17 +0000

    Went back to avoid conflicts

---
+++ scripts/gameover_canvas.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$VBoxContainer/CurrentScore.text = "Current Population: " + str($"/root/Autoload".current_score)
+	$VBoxContainer/MaxScore.text = "Your max score: " + str($"/root/Autoload".max_score)
+
+
+
+func _on_texture_button_pressed() -> void:
+	SceneSwitcher.change_scene("res://scenes/main_menu.tscn")
